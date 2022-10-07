4ff9743d855d9efb0b5eecff634221853f78324e
DzmitryKravchuk <dzmitry.kravchuk@gmail.com>
2022-10-07 16:53:40 +0300

    without Grab

--- src/ch/syseca/reportAggragator/HtmlReportReader.groovy
+++ src/ch/syseca/reportAggragator/HtmlReportReader.groovy
@@ -1,9 +1,5 @@
 package ch.syseca.reportAggragator
 
-@Grab('xml-apis:xml-apis:1.4.01')
-@Grab('org.jsoup:jsoup:1.10.2')
-
-import org.w3c.dom.ElementTraversal
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
